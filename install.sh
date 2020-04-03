7f6c86531861ccba2cb3bd730a6ac63cc481922b
yuyiwei <yuyiwei@bituniverse.org>
2020-04-03 14:54:47 +0800

    add install sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+npm install
+npm install hexo-deployer-git --save
+npm install live2d-widget-model-haru
+cp -a node_modules/live2d-widget-model-haru/package.json  node_modules/live2d-widget-model-haru/01
+hexo g

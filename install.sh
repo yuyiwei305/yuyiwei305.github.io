e5528454da78a0364e89b257d1f56ce0ca15f294
yuyiwei <yuyiwei@bituniverse.org>
2020-04-03 15:02:59 +0800

    change new girl

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@ npm install
 npm install hexo-deployer-git --save
 npm install live2d-widget-model-haru
 cp -a node_modules/live2d-widget-model-haru/package.json  node_modules/live2d-widget-model-haru/01
+cp -a node_modules/live2d-widget-model-haru/package.json  node_modules/live2d-widget-model-haru/02
 hexo g

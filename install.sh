3f887420bacc1ad2a3987bd8579b7a587e548143
yuyiwei <yuyiwei@bituniverse.org>
2020-04-10 16:25:05 +0800

    add mermaid

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 npm install
 npm install hexo-deployer-git --save
 npm install live2d-widget-model-haru
+npm install hexo-filter-mermaid-diagrams
 cp -a node_modules/live2d-widget-model-haru/package.json  node_modules/live2d-widget-model-haru/01
 cp -a node_modules/live2d-widget-model-haru/package.json  node_modules/live2d-widget-model-haru/02
 hexo g
